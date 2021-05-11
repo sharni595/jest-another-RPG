fd05dd3f0fc61425d9fd0152e808472f6fd5fa2d
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-10 18:47:20 -0600

    create player constructor function

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,4 +1,10 @@
+
+
 const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
 
 test('creates a random potion object', () => {
     const potion = new Potion();
@@ -6,4 +12,8 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
 });
